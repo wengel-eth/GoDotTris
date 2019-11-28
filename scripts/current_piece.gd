431ab8d87b91a887f385547de582d1e4a34d7fce
Schleckenmeister <s.williamengel@gmail.com>
2019-11-28 12:34:55 -0600

    Swapped some nodes
    + Changed piece Node's to Node2D's so they can move around, didn't reallize they needed it haha
    + Tick interval that drops the loaded block

--- scripts/current_piece.gd
+++ scripts/current_piece.gd
@@ -8,7 +8,13 @@ extends Node
 ###############
 ## VARIABLES ##
 
-var pieceT = load("res://scenes/sub-scenes/T-Piece.tscn").instance()
+export (int) var timer = 0
+export (float) var speed = 1
+
+var tick = 2
+var interval = 60
+
+var pieceT = preload("res://scenes/sub-scenes/T-Piece.tscn").instance()
 var pieceL = load("res://scenes/sub-scenes/L-Piece.tscn").instance()
 var pieceJ = load("res://scenes/sub-scenes/J-Piece.tscn").instance()
 var pieceS = load("res://scenes/sub-scenes/S-Piece.tscn").instance()
@@ -31,10 +37,22 @@ var pieceO = load("res://scenes/sub-scenes/O-Piece.tscn").instance()
 ## MAIN LOOP ##
 
 func _enter_tree():
+	add_child(pieceT)
+
+func _ready():
+	pass
+
+func _exit_tree():
 	pass
 
 func _process(delta):
 	pass
+
+func _physics_process(delta):
+	timer += speed
+	if timer == (interval * tick):
+		pieceT.global_translate(Vector2(0, 32))
+		timer = 0
 
 ## MAIN LOOP ##
 ###############
