bbc83c76b2b3f993a28d94536c8bb166a6a1146e
Schleckenmiester <s.williamengel@gmail.com>
2019-12-03 00:14:22 -0600

    Various tweeks and fixes
    * Fixed bug where pieces would fall through corner
    * Fixed bug where submitted pieces would display incorrectly
    * FIxed bug that wouldn't allow pieces to be placed
    * Fixed a bug that placed too many pieces
    + Added placing pieces functionality

--- scripts/current_piece.gd
+++ scripts/current_piece.gd
@@ -3,7 +3,7 @@
 ## Date Created : 2019-11-27 ##
 ###############################
 
-extends Node
+extends Node2D
 
 ###############
 ## VARIABLES ##
@@ -24,8 +24,21 @@ var pieceZ = load("res://scenes/sub-scenes/Z-Piece.tscn").instance()
 var pieceI = load("res://scenes/sub-scenes/I-Piece.tscn").instance()
 var pieceO = load("res://scenes/sub-scenes/O-Piece.tscn").instance()
 
-onready var grid = get_node(NodePath("../suroundUI"))
+onready var grid = get_node(NodePath("../surroundArea/surroundUI"))
+onready var area = get_node(NodePath("../surroundArea/pieceArea"))
+onready var leftWall = get_node(NodePath("../surroundArea/leftWall"))
+onready var rightWall = get_node(NodePath("../surroundArea/rightWall"))
+onready var topWall = get_node(NodePath("../surroundArea/topWall"))
+onready var bottomWall = get_node(NodePath("../surroundArea/bottomWall"))
+
+var unit = 32
 var currentPiece = null
+var frameOffset = Vector2()
+var frameCollision = false
+var floorCollision = false
+var floorTimer = 0
+var floorTimerMax = 60
+var hardCollision = false
 
 var pieceArray = [pieceT, pieceL, pieceJ, pieceS, pieceZ, pieceI, pieceO]
 var pieceIndex = null
@@ -46,7 +59,7 @@ var pressedTickerInterval = 0
 # gets a random float between 0 and 6 and rounds to a whole number
 func blop():
 	randomize()
-	var ib = round(rand_range(0, 6))
+	var ib = round(rand_range(-0.5, 6.499))
 	return ib
 
 # creates a random piece at the start
@@ -54,14 +67,29 @@ func spawnPiece():
 	pieceIndex = blop()
 	add_child(pieceArray[pieceIndex].duplicate())
 	currentPiece = get_child(0)
+	currentPiece.global_translate(Vector2(0, unit*-9))
+	pieceRotation = 0
 	if pieceIndex != 5 and pieceIndex != 6:
 		currentPiece.global_translate(Vector2(16, 16))
 
 # reparent a node to another node
-func reparent(source, target):
-	remove_child(source)
+func reparent(parent, source, target):
+	parent.remove_child(source)
 	target.add_child(source)
 	source.set_owner(target)
+
+# transfers each blobk to the grid
+func cement(piece):
+	for x in range(piece.get_child_count()):
+		piece.get_child(0).set_position(piece.get_child(0).get_global_position())
+		reparent(piece, piece.get_child(0), area)
+
+# all the previous functions put together to submit the piece
+func placePiece():
+	reparent(self, currentPiece, area)
+	cement(currentPiece)
+	currentPiece.queue_free()
+	spawnPiece()
 
 ## FUNCTIONS ##
 ###############
@@ -69,32 +97,34 @@ func reparent(source, target):
 ###############
 ## MAIN LOOP ##
 
-func _enter_tree():
-	pass
-
 func _ready():
 	spawnPiece()
 
-func _exit_tree():
-	pass
-
 func _process(delta):
-	pass
-
-func _physics_process(delta):
 	# checks if both keys are pressed and exits if they are
 	if Input.is_action_pressed("move_left") and Input.is_action_pressed("move_right"):
 		pass
 	else:
 		# checks if left button is pressed, moves piece left and continues if still pressed
 		if Input.is_action_just_pressed("move_left"):
-			currentPiece.global_translate(Vector2(-32, 0))
-			leftPressed = true
+			for i in range(currentPiece.get_child_count()):
+				if currentPiece.get_child(i).overlaps_area(leftWall):
+					frameCollision = true
+					break
+			if frameCollision == false:
+				frameOffset += Vector2(unit*-1, 0)
+				leftPressed = true
+			floorCollision = false
 		if Input.is_action_pressed("move_left") and leftPressed:
 			if leftPressedTicker == pressedTickerMax:
 				if pressedTickerInterval == pressedTickerIntervalMax:
-					currentPiece.global_translate(Vector2(-32, 0))
-					pressedTickerInterval = 0
+					for s in range(currentPiece.get_child_count()):
+						if currentPiece.get_child(s).overlaps_area(leftWall):
+							frameCollision = true
+							break
+					if frameCollision == false:
+						frameOffset += Vector2(unit*-1, 0)
+						pressedTickerInterval = 0
 				else:
 					pressedTickerInterval += 1
 			else:
@@ -106,13 +136,24 @@ func _physics_process(delta):
 		
 		# checks if right button is pressed, moves piece right and continues if still pressed
 		if Input.is_action_just_pressed("move_right"):
-			currentPiece.global_translate(Vector2(32, 0))
-			rightPressed = true
+			for a in range(currentPiece.get_child_count()):
+				if currentPiece.get_child(a).overlaps_area(rightWall):
+					frameCollision = true
+					break
+			if frameCollision == false:
+				frameOffset += Vector2(unit, 0)
+				rightPressed = true
+			floorCollision = false
 		if Input.is_action_pressed("move_right") and rightPressed:
 			if rightPressedTicker == pressedTickerMax:
 				if pressedTickerInterval == pressedTickerIntervalMax:
-					currentPiece.global_translate(Vector2(32, 0))
-					pressedTickerInterval = 0
+					for r in range(currentPiece.get_child_count()):
+						if currentPiece.get_child(r).overlaps_area(rightWall):
+							frameCollision = true
+							break
+					if frameCollision == false:
+						frameOffset += Vector2(unit, 0)
+						pressedTickerInterval = 0
 				else:
 					pressedTickerInterval += 1
 			else:
@@ -133,17 +174,41 @@ func _physics_process(delta):
 		# gets if right button was pressed and rotates if true
 		if Input.is_action_just_pressed("rotate_right"):
 			pieceRotation += 90
-		
-		currentPiece.set_global_rotation(deg2rad(pieceRotation))
 	
+	# checks if space is pressed, and does a hard drop if so
 	if Input.is_action_just_pressed("hard_drop"):
-		reparent(currentPiece, grid)
-		spawnPiece()
+		placePiece()
 	
+	# checks for collision and moves
+	for i in range(currentPiece.get_child_count()):
+		if currentPiece.get_child(i).overlaps_area(bottomWall):
+			floorCollision = true
+		for g in range(area.get_child_count()):
+			if currentPiece.get_child(i).overlaps_area(area.get_child(g)):
+				floorCollision = true
+	
+	# gradual moving down
 	timer += speed
-	if timer == (interval * tick):
-		currentPiece.global_translate(Vector2(0, 32))
+	if timer >= (interval * tick) and floorCollision == false:
+		currentPiece.global_translate(Vector2(0, unit))
 		timer = 0
+	if floorCollision:
+		floorTimer += 1
+		if floorTimer <= floorTimerMax:
+			pass
+		else:
+			placePiece()
+			floorCollision = false
+			floorTimer = 0
+			timer = 0
+	else:
+		floorTimer = 0
+	
+	# moves collision piece
+	currentPiece.global_translate(frameOffset)
+	currentPiece.set_global_rotation(deg2rad(pieceRotation))
+	frameOffset = Vector2()
+	frameCollision = false
 
 ## MAIN LOOP ##
 ###############
